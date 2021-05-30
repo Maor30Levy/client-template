6164f99b90ef12b2e81f330d74517f88e1249e44
Maor30Levy <75747578+Maor30Levy@users.noreply.github.com>
2021-05-30 21:35:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,31 +3,15 @@ const cors = require('cors');
 const hbs = require('hbs');
 
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 const server = require('http').createServer(app);
 app.use(express.json());
 app.use(cors());
 
 const publicDirectory = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
-
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
-hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectory));
-
-
-
-app.get('', (req, res) => {
-    res.render('index', {
-        name: "Maor Levy",
-        host: req.get('host')
-    });
-});
-
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
